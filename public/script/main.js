951e25ead3d7bcd33afd374977fc2038f75e59b3
Denisdhl007 <denis.henin@icloud.com>
2025-01-16 11:55:00 +0100

    version_3

--- public/script/main.js
+++ public/script/main.js
@@ -1,3 +1,3 @@
 const footerText = document.querySelector('.footer-content p');
 const currentYear = new Date().getFullYear();
-footerText.innerHTML = `&copy; ${currentYear} LinkTree. Tous droits réservés.`;
+footerText.innerHTML = `&copy; ${currentYear} LinkTree. All Rights Reserved.`;
